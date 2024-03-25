d9673d77fc000868a8e706671cd3153226b93e65
nway0580q <nway.sandi.oo@ihis.com.sg>
2024-03-25 17:43:09 +0800

    initial test OpenIddict

---
+++ AuthServer/Models/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthServer.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public string? ReturnUrl { get; set; }
+    }
+}
---
+++ AuthServer/Program.cs
@@ -0,0 +1,113 @@
+using AuthServer;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+        .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+        {
+            options.LoginPath = "/account/login";
+        });
+builder.Services.AddDbContext<DbContext>(options =>
+{
+    // Configure the context to use an in-memory store.
+    options.UseInMemoryDatabase(nameof(DbContext));
+    // Register the entity sets needed by OpenIddict.
+    options.UseOpenIddict();
+});
+
+/*builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    // Configure the Entity Framework Core to use Microsoft SQL Server.
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+    // Register the entity sets needed by OpenIddict.
+    options.UseOpenIddict();
+});*/
+
+builder.Services.AddOpenIddict()
+
+        // Register the OpenIddict core components.
+        .AddCore(options =>
+        {
+            // Configure OpenIddict to use the EF Core stores/models.
+            options.UseEntityFrameworkCore()
+                .UseDbContext<DbContext>();
+        })
+
+        //Use ApplicationDbContext
+        /*.AddCore(options =>
+        {
+            // Configure OpenIddict to use the EF Core stores/models.
+            options.UseEntityFrameworkCore()
+                .UseDbContext<ApplicationDbContext>();
+        })*/
+        // Register the OpenIddict server components.
+        .AddServer(options =>
+        {
+            options
+                .AllowClientCredentialsFlow();
+            options.AllowAuthorizationCodeFlow().RequireProofKeyForCodeExchange();
+
+
+            options
+            .SetAuthorizationEndpointUris("/connect/authorize")
+                .SetTokenEndpointUris("/connect/token");
+
+            // Encryption and signing of tokens
+            options
+                .AddEphemeralEncryptionKey()
+                .AddEphemeralSigningKey();
+
+            // Register scopes (permissions)
+            options.RegisterScopes("api");
+
+            // Register the ASP.NET Core host and configure the ASP.NET Core-specific options.
+            options
+                .UseAspNetCore()
+                .EnableTokenEndpointPassthrough()
+            .EnableAuthorizationEndpointPassthrough();
+            options
+                .AddEphemeralEncryptionKey()
+                .AddEphemeralSigningKey()
+                .DisableAccessTokenEncryption();
+        });
+
+builder.Services.AddHostedService<TestData>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseAuthentication();
+
+//app.MapRazorPages();
+//app.MapDefaultControllerRoute();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+});
+
+/*app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");*/
+
+app.Run();
