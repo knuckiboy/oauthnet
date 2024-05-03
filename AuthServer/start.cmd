b67fd3daeb9bfb2521b1d3efd8d6d7c320171ab8
Jian Nan Ng <ng.jian.nan@synapxe.sg>
2024-05-03 08:11:42 +0800

    fix image name

--- AuthServer/start.cmd
+++ AuthServer/start.cmd
@@ -5,7 +5,7 @@ rem create a self signed cert
 rem dotnet dev-certs https -ep %USERPROFILE%\.aspnet\https\aspnetapp.pfx -p password
 rem dotnet dev-certs https --trust
 docker build -t hh_dev_ciam_test .
-docker rm -f hh_dev_ciam_test
-docker run --name hh_dev_ciam_test --rm -it -p 8000:80 -p 8001:443 -e ASPNETCORE_URLS="https://+:443;http://+:80" -e ASPNETCORE_HTTPS_PORTS=8001 -e ASPNETCORE_ENVIRONMENT="Production" -e ASPNETCORE_Kestrel__Certificates__Default__Password="password" -e ASPNETCORE_Kestrel__Certificates__Default__Path=/https/aspnetapp.pfx -v %USERPROFILE%\.aspnet\https:/https/ auth-poc
+docker rm -f hh_dev_ciam_test_img
+docker run --name hh_dev_ciam_test_img --rm -it -p 8000:80 -p 8001:443 -e ASPNETCORE_URLS="https://+:443;http://+:80" -e ASPNETCORE_HTTPS_PORTS=8001 -e ASPNETCORE_ENVIRONMENT="Production" -e ASPNETCORE_Kestrel__Certificates__Default__Password="password" -e ASPNETCORE_Kestrel__Certificates__Default__Path=/https/aspnetapp.pfx -v %USERPROFILE%\.aspnet\https:/https/ hh_dev_ciam_test
 
 pause
